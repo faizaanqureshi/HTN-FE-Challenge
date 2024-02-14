282f919096ed175071843fcf181b06a17a3ba1d7
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-13 20:12:03 -0500

    add fool proof sorting based on start time

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { TEvent } from '../types';
 export default async function Home() {
   let eventData = await axios.get('https://api.hackthenorth.com/v3/events')
   let events: TEvent[] = eventData.data;
+  events = events.sort((a, b) => a.start_time - b.start_time);
 
   return (
     <main className="flex min-h-screen flex-col items-center p-4">
